cf1ebe903529325207a0f5d2926d1887aa92635d
KainatAli080 <162943891+KainatAli080@users.noreply.github.com>
2024-03-14 20:30:52 +0500

    Time: 4 ms (37.78%), Space: 7.2 MB (26.82%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t reversedNum = 0;
+        for(int i=0; i<32; i++){
+            reversedNum = reversedNum << 1;
+            int leastSignificantBit = n&1;
+            reversedNum |= leastSignificantBit;
+            n >>=1;
+        }
+        return reversedNum;
+    }
+};
