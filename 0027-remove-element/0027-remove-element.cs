2d4764f59fd222d5ba765e326e2a235b3729bccf
KainatAli080 <162943891+KainatAli080@users.noreply.github.com>
2025-08-10 22:55:33 +0500

    Time: 0 ms (100%), Space: 47.1 MB (24.07%) - LeetHub

--- 0027-remove-element/0027-remove-element.cs
+++ 0027-remove-element/0027-remove-element.cs
@@ -1,8 +1,10 @@
 public class Solution {
     public int RemoveElement(int[] nums, int val) {
         int s = nums.Length;
-        int k = 0;
 
+        // we're looping through the array, decreasing s when the element to be removed is added to the end
+        // no need to check the end
+        // if end checked, swapping would be messed with
         for(int i = 0; i < s;){
             if(nums[i] == val){                
                 nums[i] = nums[s-1];
